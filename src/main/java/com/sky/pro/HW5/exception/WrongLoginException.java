eb6e6d36df6c1afe1de9c81a70f4d21a29c7f2f8
Elena <elenela@yandex.ru>
2023-06-17 11:22:16 +0300

    Домашняя работа 5!

--- src/main/java/com/sky/pro/HW5/exception/WrongLoginException.java
+++ src/main/java/com/sky/pro/HW5/exception/WrongLoginException.java
@@ -1,11 +1,6 @@
-
 package com.sky.pro.HW5.exception;
-
 public class WrongLoginException extends RuntimeException {
-
-
     public WrongLoginException(String message) {
-
         super(message);
     }
 }
