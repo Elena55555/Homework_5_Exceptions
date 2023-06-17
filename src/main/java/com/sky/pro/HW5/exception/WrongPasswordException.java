a6eabd3a315aac1c44e25cd4f2efee6c92b5008c
Elena <elenela@yandex.ru>
2023-06-17 17:32:03 +0300

    Домашняя работа 5 (курс 2) c изменениями

--- src/main/java/com/sky/pro/HW5/exception/WrongPasswordException.java
+++ src/main/java/com/sky/pro/HW5/exception/WrongPasswordException.java
@@ -1,14 +1,5 @@
-
-
-
 package com.sky.pro.HW5.exception;
-
-
-
 public class WrongPasswordException extends RuntimeException {
-
-
-
     public WrongPasswordException(String message) {
         super(message);
     }
